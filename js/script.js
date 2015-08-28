aeb4d76da15d909f82ef47fe0e4d6f91c35159de
Daniel Hug <daniel@hugwebdesign.com>
2015-08-28 14:24:07 -0700

    Fix li DOM building

--- js/script.js
+++ js/script.js
@@ -3,23 +3,34 @@
 $(document).ready(function(){
 
 	function addItemToList(item){
-		// var li = '<li class="checkbox"><div class="row"><div class="col-xs-8"><label><input type="checkbox" value="">'+item+'</label></div><div class="col-xs-4"><button class="delete btn btn-warning btn-xs" type="submit">Delete</button></div></div></li>';
 		var li = DOM.buildNode({
-	    	el: 'l1',
-	    	kids: [
-	    		{
-			    	el: 'div',
-			    	_className: 'row'
-	    		}
-	    		kids: [
-		    		{
-		    			el:'div',
-		    			_className: 'col-xs-8'
-		    		}
-	    			]
-	    	]
-		});
-	    	]
+			el: 'li',
+			kids: [{
+				_className: 'row',
+				kids: [
+					{
+						_className: 'col-xs-8',
+						kids: [{
+							el: 'label',
+							kids: [
+								{
+									el: 'input',
+									type: 'checkbox'
+								},
+								item
+							]
+						}]
+					},
+					{
+						_className: 'col-xs-4',
+						kids: [{
+							el: 'button',
+							_className: 'delete btn btn-warning btn-xs',
+							kids: ['Delete']
+						}]
+					}
+				]
+			}]
 		});
 		$("#list").append(li);
 	}
@@ -29,13 +40,9 @@ $(document).ready(function(){
 		event.preventDefault();
 	});
 
-	$(document).on("click", ".delete", function(e){
-		e.preventDefault();
-		$(this).parent().parent().parent().remove();
-	});	
-
-	var li = DOM.buildNode({
-    	kids: 'li'
+	$(".delete").click(function(){
+		$(li).remove();
+		// event.preventDefualt isn't needed 'cause this button doesn't submit a form
 	});
 });
 
